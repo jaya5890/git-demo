e588ff27ff4d455039251f0f2e27c82ab2df3285
mohangowda18 <mohangowda18@gmail.com>
2020-09-04 12:03:29 +0530

    first commit

---
+++ Git-demo/src/main/java/com/jaya/Git_demo/App.java
@@ -0,0 +1,14 @@
+package com.jaya.Git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+       
+    }
+}
